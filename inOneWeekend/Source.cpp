8a3be0c43c8035f58869409089fa1cf3f22c9ec7
darkestofbreads <unprofessional.nouzee@gmail.com>
2022-11-08 22:28:11 +0100

    Added Lighting! For now, the only light source available is a material which causes objects to emit light. Later, directional light sources come in which will be important for simulating the sun. I will eventually look into point light sources too and deep dive into blenders types of light sources. (no garuntees tho that i will actually care about the latter part going forward)

--- inOneWeekend/Source.cpp
+++ inOneWeekend/Source.cpp
@@ -19,7 +19,6 @@
 //		- light sources:
 //			- point light sources which should be pretty straight forward
 //			- directional light sources (here comes the sun)
-//			- emissive objects, which are covered first due to it being covered in the book (basically just another material)
 // 
 //		- normal maps, roughness and metallic maps and so on (a good opportunity to assure blender "compatibility")
 // 
@@ -31,11 +30,11 @@
 // textures will only fully work on spheres, they wont work at all on boxes and on triangles only when
 // boxes arent present (tldr: boxes make kaput pictures)
 
-color rayColor(const ray& r, const hittable& world, int depth);
+color rayColor(const ray& r, const color& background, const hittable& world, int depth);
 
 //yes i'm aware of how awful this looks atm
-void startRender(const int imageWidth, float aspectRatio, const int samples, const int bounces, hittable_list& world, const int processorCount, camera& cam);
-std::vector<int> render(int imageWidth, int imageHeight_steps, int imageHeight, int j, int samples, int bounces, const hittable_list& world, const camera& cam);
+void startRender(const int imageWidth, float aspectRatio, const color& background, const int samples, const int bounces, hittable_list& world, const int processorCount, camera& cam);
+std::vector<int> render(int imageWidth, int imageHeight_steps, int imageHeight, int j, const color& background, int samples, int bounces, const hittable_list& world, const camera& cam);
 
 int WinMain() {
 	
@@ -45,15 +44,16 @@ int WinMain() {
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	
 	// Image quality
-	const int samples_per_pixel = 50;
+	const int samples_per_pixel = 500;
 	const int bounces = 20;
-	const bool doDepthOfField = false;
+	const bool doDepthOfField = true;
+	const color background(0.01, 0.01, 0.01);
 
 	// Get CPU specs
 	const auto processor_count = std::thread::hardware_concurrency();
 
 	// Camera
-	point3 lookfrom(0, 0, 3);
+	point3 lookfrom(0, 2, 4);
 	point3 lookat(3, 0, -1);
 	vec3 vup(0, 1, 0);
 
@@ -70,7 +70,7 @@ int WinMain() {
 	ray r = cam.get_ray(image_width / 2, image_height / 2);
 	if (!world.hit(r, 0.001, infinity, rec) && doDepthOfField) {
 		float defocusDist = vec3(lookfrom - rec.p).length() + 1;
-		cam.setDoF(defocusDist, lensRadius);
+		cam.setDoF(defocusDist, lensRadius/defocusDist);
 	}
 
 	auto material_ground = make_shared<metal>(color(0.4, 0.9, 0.0), 0);
@@ -80,58 +80,62 @@ int WinMain() {
 	auto material_left = make_shared<metal>(color(0.8, 0.6, 0.2), 0.05);
 	auto dielectric0 = make_shared<dielectric>(1.5);
 	auto dielectric1 = make_shared<dielectric>(3);
+	auto whitelight = make_shared<diffuse_light>(color(0,4,0.7));
 	auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
 
 	auto some_texture = make_shared<image_texture>("earthmap.jpg");
+	auto crappy_bricks = make_shared<image_texture>("crappy_bricks.png");
+
+	auto crappy_light = make_shared<diffuse_light>(crappy_bricks);
+
 	auto some_surface = make_shared<lambertian>(some_texture);
+	auto some_surface_light = make_shared<diffuse_light>(some_texture);
 
 	//CAUTION: Adding hittables currently increases render time by a lot! (remove when bounding boxes are implemented)
 
 	//		spheres:					(x, y, z), radius, material
 	//		triangles:			 point3 a, point3 b, point3 c, material
-	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.6, some_surface));
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.6, material_center));
 	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_left));
-	//world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
 	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_center));
 	world.add(make_shared<sphere>(point3(-2, 0, -2), 0.5, dielectric0));
 	world.add(make_shared<sphere>(point3(-1, 0, -1.5), -0.35, dielectric1));
 	world.add(make_shared<sphere>(point3(-1, 0, -1.5), 0.3, dielectric1));
-	world.add(make_shared<sphere>(point3(3, 1, -2), 0.8, material_center));
+	world.add(make_shared<sphere>(point3(-2, 3, -5), 0.4, whitelight));
+	world.add(make_shared<sphere>(point3(5, 2, -2), 0.6, crappy_light));
 	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, make_shared<lambertian>(checker)));
-	//world.add(make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), material_center));
-	//world.add(make_shared<box>(point3(3, -1, -2), point3(4, 3, 2), material_ground));
 	world.add(make_shared<triangle>(point3(1, 0, -4), point3(2, 0, -6), point3(3, 3, -7), material_center));
 	world.add(make_shared<triangle>(point3(2, -2, -4), point3(3, -1, 0), point3(2.5, 3, -3), some_surface));
-	world.add(make_shared<triangle>(point3(-3, -1, 0), point3(2, -2, -4), point3(2.5, 3, -3), material_center));
+	world.add(make_shared<triangle>(point3(-3, -1, 0), point3(2, -2, -4), point3(2.5, 3, -3), some_surface_light));
 
-	startRender(image_width, aspect_ratio, samples_per_pixel, bounces, world, processor_count, cam);
+	startRender(image_width, aspect_ratio, background, samples_per_pixel, bounces, world, processor_count, cam);
 
 	return 0;
 }
 
 //i should probably start understanding this function before continuing with adding rt related features
-color rayColor(const ray& r, const hittable& world, int bounces) {
+color rayColor(const ray& r, const color& background, const hittable& world, int bounces) {
 	hit_record rec;
 
 	if (bounces <= 0)
 		return color(0, 0, 0);
 
-	if (world.hit(r, 0.001, infinity, rec)) {
-		ray scattered;
-		color attenuation;
-		if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-			return attenuation * rayColor(scattered, world, bounces - 1);
-		return color(0, 0, 0);
-	}
+	if (!world.hit(r, 0.001, infinity, rec))
+		return background;
 
-	//i think this it the background? if it is then change 
-	vec3 unit_direction = unit_vector(r.direction());
-	auto t = 0.5f * (unit_direction.y() + 1.0f);
-	return (1.0f - t) * color(1.0f, 1.0f, 1.0f) + t * color(0.5f, 0.7f, 1.0f);
+	ray scattered;
+	color attenuation;
+	color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+
+	if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+		return emitted;
+
+	//this is our background
+	return emitted + attenuation * rayColor(scattered, background, world, bounces - 1);
 }
 
 //this part below is actually fully written by me and not entirely copy-pasted
-void startRender(const int imageWidth, float aspectRatio, const int samples, const int bounces, hittable_list& world, const int processorCount, camera& cam) {
+void startRender(const int imageWidth, float aspectRatio, const color& background, const int samples, const int bounces, hittable_list& world, const int processorCount, camera& cam) {
 	std::vector<std::future<std::vector<int>>> ftr;
 	std::vector<std::vector<int>> imageData;
 
@@ -145,7 +149,7 @@ void startRender(const int imageWidth, float aspectRatio, const int samples, con
 	//to pass something by reference here, std::ref or std::cref (for constant stuff, hence the c) needs to be used
 	for (int pos = processorCount-1; pos >= 0; pos--)
 		//ridiculous amount of parameters but oh well
-		ftr.push_back(std::async(render, imageWidth, heightPixelSteps, imageHeight, pos + 1, samples, bounces, std::cref(world), std::cref(cam)));
+		ftr.push_back(std::async(render, imageWidth, heightPixelSteps, imageHeight, pos + 1, std::cref(background), samples, bounces, std::cref(world), std::cref(cam)));
 
 	//future of vector of int to vector of int
 	for (auto& oc : ftr)
@@ -162,7 +166,7 @@ void startRender(const int imageWidth, float aspectRatio, const int samples, con
 	stbi_write_png("image.png", imageWidth, imageHeight, 3, image, imageWidth*3);
 }
 
-std::vector<int> render(int imageWidth, int imageHeight_steps, int imageHeight, int j, int samples, int bounces, const hittable_list& world, const camera& cam) {
+std::vector<int> render(int imageWidth, int imageHeight_steps, int imageHeight, int j, const color& background, int samples, int bounces, const hittable_list& world, const camera& cam) {
 	std::vector<int> image;
 	auto scale = 1.0 / samples;
 
@@ -176,7 +180,7 @@ std::vector<int> render(int imageWidth, int imageHeight_steps, int imageHeight, 
 				auto u = (b + random_float()) / (imageWidth - 1);
 				auto v = (a + random_float()) / (imageHeight - 1);
 				ray r = cam.get_ray(u, v);
-				pixelColor += rayColor(r, world, bounces);
+				pixelColor += rayColor(r, background, world, bounces);
 			}
 
 			//allocate pixels to output string
--- inOneWeekend/material.h
+++ inOneWeekend/material.h
@@ -13,6 +13,27 @@ public:
 	virtual bool scatter(
 		const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
 	) const = 0;
+
+	virtual color emitted(float u, float v, const point3& p) const {
+		return color(0, 0, 0);
+	}
+};
+
+class diffuse_light : public material {
+public:
+	diffuse_light(shared_ptr<texture> a) : emit(a) {}
+	diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+
+	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+		const override {
+		return false;
+	}
+
+	virtual color emitted(float u, float v, const point3& p) const override {
+		return emit->value(u, v, p);
+	}
+public:
+	shared_ptr<texture> emit;
 };
 
 class lambertian : public material {
